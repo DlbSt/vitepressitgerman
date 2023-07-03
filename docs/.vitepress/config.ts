7b7b9a1b8c938b3626d71430246e96f5362030f1
DlbSt <stysda@gmail.com>
2023-07-03 20:58:25 +0200

    [add] First version

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,6 +7,13 @@ export default defineConfig({
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
 
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'English words', link: '/englishwords' },
+      { text: 'German words', link: '/germanwords' },
+      { text: 'German verbs', link: '/germanverbs' }
+    ],
+
     socialLinks: [
       { icon: 'twitter', link: 'https://twitter.com/DaliborStys' }, 
       { icon: 'github', link: 'https://github.com/DlbSt/vitepressitgerman' }
